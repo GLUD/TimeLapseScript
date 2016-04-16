37ae89b06cde91e3ec5ea81cf475a8cf38fe84e7
GLUD <naturalmentejorge@gmail.com>
2016-04-16 11:14:22 -0500

    Se mejora el script para que se ejecute cada 10 segundos en el script por 5 veces y cada minuto en crotab

--- script.sh
+++ script.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 #http://cecs.wright.edu/~pmateti/Courses/233/Labs/Scripting/bashVsPowerShellTable.html
 devCam=$(ls /dev/v4l/by-id/usb-* | head -1)
-directorio=/home/jorge2/Descargas/fotos
+#directorio=$HOME/Pictures/timelapse
+dirDia=`date +%Y-%m-%d`
+directorio=/home/glud/Pictures/timelapse/$dirDia
 if ! [ -d "$directorio" ]; then
  mkdir $directorio
+ chown glud:glud $directorio -r
 fi
-ffmpeg -f video4linux2 -i $devCam -vframes 1 $directorio/pic_`date +%F_%T`.jpeg
+
+function tomarFoto {
+ nombreArchivo=$directorio/pic_`date +%F_%T`.jpeg
+ ffmpeg -f video4linux2 -i $devCam -vframes 1 $nombreArchivo
+ chown glud:glud $nombreArchivo
+}
+
+for i in `seq 1 5`; do
+tomarFoto
+sleep 10
+done
