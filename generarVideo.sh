c76c6dd42c3cd764ac134509e613d0152b049944
Jorge Useche <naturalmentejorge@gmail.com>
2016-04-18 15:49:11 -0500

    Mejorando para realiar los vídeos automáticamente

---
+++ generarVideo.sh
@@ -0,0 +1,1 @@
+ffmpeg -framerate 24 -pattern_type glob -i "pic_*.jpeg" video.webm
---
+++ tomarFotos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#http://cecs.wright.edu/~pmateti/Courses/233/Labs/Scripting/bashVsPowerShellTable.html
+#Variables de configuración:
+#directorio donde desea almacenar las imágenes
+directorio=/home/glud/Pictures/timelapse/
+#usuario y grupo de quien es propietario la carpeta
+usuario="glud"
+grupo="glud"
+
+#Se obtiene el primer recurso de cámara detectado por el sistema operativo
+devCam=$(ls /dev/v4l/by-id/usb-* | head -1)
+#directorio=$HOME/Pictures/timelapse
+#directorio="/home/jorge2/Imágenes/timelapse/$dirDia"
+dirDia=`date +%Y-%m-%d`
+directorio="$directorio$dirDia"
+if ! [ -d "$directorio" ]; then
+ mkdir $directorio
+ chown "$usuario:$grupo" $directorio -R
+fi
+
+function tomarFoto {
+ nombreArchivo=$directorio/pic_`date +%F_%T`.jpeg
+ ffmpeg -f video4linux2 -i $devCam -vframes 1 $nombreArchivo
+ chown "$usuario:$grupo" $nombreArchivo
+}
+
+for i in `seq 1 5`; do
+tomarFoto
+sleep 10
+done
