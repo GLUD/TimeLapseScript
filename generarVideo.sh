88f71cf3744a806c4e8b3407cc0424d1b7e4bd7e
GLUD <naturalmentejorge@gmail.com>
2016-04-19 13:34:58 -0500

    se generan vídeos con parametros de entrada

--- generarVideo.sh
+++ generarVideo.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+#directorio donde desea almacenar las imágenes
+directorio=/home/glud/Pictures/timelapse/
+#dia="2016-04-18"
+dia=$1
+#comienza script
+directorio="$directorio$dia"
+cd $directorio
 ffmpeg -framerate 24 -pattern_type glob -i "pic_*.jpeg" video.webm
